8ddef98b49cc49c7ece8fa1d48cb52ac922444a9
richiemtx <21040361@itdurango.edu.mx>
2024-12-18 15:52:03 -0600

    Add files via upload

---
+++ public/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc } from "@firebase/firestore"
+
+console.log("addDoc: ", addDoc);
+console.log("collection ", collection);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBqyMAMByTX0b0bqvHu-o9IoJSG3voYjsw",
+  authDomain: "mambo-4f8ba.firebaseapp.com",
+  projectId: "mambo-4f8ba",
+  storageBucket: "mambo-4f8ba.firebasestorage.app",
+  messagingSenderId: "326320108967",
+  appId: "1:326320108967:web:2076ef0192aebbc7657282"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
---
+++ src/reportWebVitals.js
@@ -0,0 +1,7 @@
+// src/reportWebVitals.js
+const reportWebVitals = () => {
+    console.log("Web vitals are being reported");
+  };
+  
+  export default reportWebVitals;
+  
